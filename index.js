60d1f7d18715a6be0855491b75c89be7dfff2a96
Joshua Berja <50904036+joshberja@users.noreply.github.com>
2023-08-14 20:20:38 -0700

    Add error messaging behavior

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const submitButton = document.getElementById("submit");
 const rateAgain = document.getElementById("rate-again");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
+const userError = document.querySelector(".no-rating-selected");
 
 submitButton.addEventListener("click", () => {
-    thanksContainer.classList.remove("hidden");
+  if (rating.innerText === "") {
+    userError.style.display = "block";
+  } else {
+    thanksContainer.classList.remove("thank-you-hidden");
     mainContainer.style.display = "none";
+  }
 });
 
 rateAgain.addEventListener("click", () => {
-    thanksContainer.classList.add("hidden");
-    mainContainer.style.display = "block";
+  thanksContainer.classList.add("thank-you-hidden");
+  mainContainer.style.display = "block";
+  //   submitButton.disabled = true;
+  rating.innerText = "";
+  userError.style.display = "none";
 });
 
 rates.forEach((rate) => {
-    rate.addEventListener("click", () => {
-    //  console.log("Rating: " + rate.innerHTML);   
-    rating.innerHTML = rate.innerHTML;
-    });
+  rate.addEventListener("click", () => {
+    rating.innerText = rate.innerText;
+    userError.style.display = "none";
+  });
 });
